22c3bbf09ddebb2b9a16e5fd2c2cec7efc2920dc
Guilherme Viana <guibviana@icloud.com>
2023-12-06 12:11:17 +0000

    started project and set for viewcode

---
+++ flash-chat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  flash-chat
+//
+//  Created by Guilherme Viana on 06/12/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
